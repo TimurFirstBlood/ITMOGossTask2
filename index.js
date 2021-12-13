0cd49e5eb4d5798dd4061f15b8467271859e4b8a
Timur Khalilev <tkhalilev@firstblood.io>
2021-12-13 15:01:44 +0300

    week 5

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-const http = require('http');
+import express from 'express'
+import bodyParser from 'body-parser'
+import {createReadStream} from 'fs'
+import crypto from 'crypto'
+import http from 'http'
 
-http
-  .Server((req, res) => {
+import appSrc from './app.js'
 
-    if (req.url === '/result4/') {
-      let CORS = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test'
+const app = appSrc(express, bodyParser, createReadStream, crypto, http)
 
-      };
-
-      const result = {
-        message: 'itmo273328',
-        'x-result': req.headers['x-test'],
-      };
-      let body = '';
-
-      req
-        .on('data', data => (body += data))
-        .on('end', () => {
-          result['x-body'] = body;
-          res.writeHead(200, {... CORS, 'Content-Type': 'application/json' });
-          res.end(JSON.stringify(result));
-        });
-    }
-  })
-  .listen(process.env.PORT);
+app.listen(process.env.PORT)
