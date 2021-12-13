c319bec74ff4dbfd19de50dceeff111af7839903
Timur Khalilev <tkhalilev@firstblood.io>
2021-12-13 15:41:16 +0300

    ej

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 
-export default function appScr(express, bodyParser, fs, crypto, http, CORS, User, m) {
+export default function appScr(express, bodyParser, fs, crypto, http, CORS, User, m, puppeteer) {
     const app = express();
     const path = import.meta.url.substring(7);
     const headersHTML = {'Content-Type':'text/html; charset=utf-8',...CORS}
     const headersTEXT = {'Content-Type':'text/plain',...CORS}
     const headersJSON={'Content-Type':'application/json',...CORS}
-    const headersCORS={...CORS}; 
 
     app    
         .use(bodyParser.urlencoded({extended:true}))  
         .use(bodyParser.json()) 
         .all('/login/', r => {
-            r.res.set(headersTEXT).send('itmo273328');
+            r.res.set(headersTEXT).send('id71293382');
         })
         .all('/code/', r => {
             r.res.set(headersTEXT)
@@ -56,21 +55,22 @@ export default function appScr(express, bodyParser, fs, crypto, http, CORS, User
                 console.log(e.codeName);
             }      
         })
-        .all('/render/',async(req,res)=>{
-            res.set(headersCORS);
-            const {addr} = req.query;
-            const {random2, random3} = req.body;
+        .all('/test/', async r=>{
+            r.res.set(headersTEXT)
+            const {URL} = r.query;
+            console.log(URL)
+            const browser = await puppeteer.launch({headless: true, args:['--no-sandbox','--disable-setuid-sandbox']});
+            const page = await browser.newPage();
+            await page.goto(URL);
+            await page.waitForSelector("#inp");
+            await page.click('#bt');
+            const got = await page.$eval('#inp',el=>el.value);
+            console.log(got);
+            browser.close()
+            r.res.send(got)
             
-            http.get(addr,(r, b='') => {
-                r
-                .on('data',d=>b+=d)
-                .on('end',()=>{
-                    fs.writeFileSync('views/index.pug', b);
-                    res.render('index',{login:'itmo273328',random2,random3})
-                })
-            })
         })
-        .use(({res:r})=>r.status(404).set(headersHTML).send('itmo273328'))
+        .use(({res:r})=>r.status(404).set(headersHTML).send('id71293382'))
         .set('view engine','pug')
     return app;
 }
--- index.js
+++ index.js
@@ -5,11 +5,12 @@ import bodyParser from 'body-parser';
 import m from 'mongoose';
 import crypto from 'crypto';
 import http from 'http';
+import puppeteer from 'puppeteer'
 import CORS from './CORS.js';
 import UserModel from './models/User.js';
 const User = UserModel(m);
-const app = appSrc(express, bodyParser, fs, crypto, http, CORS, User, m);
-const PORT = process.env.PORT || 9000;
+const app = appSrc(express, bodyParser, fs, crypto, http, CORS, User, m, puppeteer);
+const PORT = process.env.PORT || 443;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
 
 
